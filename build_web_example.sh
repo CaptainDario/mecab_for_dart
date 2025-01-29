f239235cd5dc0cc59d8965a5db9096707a626d39
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-01-29 12:23:36 +0900

    removed mecab_for_flutter files

--- build_web_example.sh
+++
@@ -1,9 +0,0 @@
-cd example
-sed -i '' '/assets\/ipadic\//s/^/#/' pubspec.yaml
-flutter build web --base-href "/mecab_for_dart/"
-sed -i '' '/assets\/ipadic\//s/^#//' pubspec.yaml
-cd ..
-
-rm -rf ./docs/
-mkdir docs
-cp -r example/build/web/ docs/
