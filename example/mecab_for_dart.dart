ee715379548ac0e670cd92934b12f338c6b5975f
CaptainDario @ MBP M1 <daapplab@gmail.com>
2026-02-08 18:24:09 +0100

    updated for new api

--- example/mecab_for_dart.dart
+++ example/mecab_for_dart.dart
@@ -4,7 +4,7 @@ import 'package:mecab_for_dart/mecab_dart.dart';
 
 void main() async {
   
-  final tagger = await Mecab.create("mecab.dylib", "ipadic", true);
+  final tagger = await Mecab.create("mecab.dylib", "ipadic", "-Owakati");
 
   final surfaces = tagger.parse("林檎を食べる").map((e) => e.surface,).toList();
   print(surfaces);
