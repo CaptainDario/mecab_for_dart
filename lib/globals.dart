071b5364c96d4cba499fa1762cfd908a3997a6e3
CaptainDario @ MBP M1 <daapplab@gmail.com>
2026-02-22 15:10:42 +0100

    corrected default options

--- lib/globals.dart
+++ lib/globals.dart
@@ -3,7 +3,7 @@
 
 /// Default options used when creating a Mecab instance if no options are
 /// provided.
-const String defaultOptions = "-Owakati";
+const String defaultOptions = "";
 
 /// Default path to the Mecab dynamic library used on web when creating a Mecab
 const String defaultWebLibMecabPath = "assets/blobs/libmecab.js";
